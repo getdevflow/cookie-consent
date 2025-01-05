dd08071365a96a70e494634a54f6a11506b0f5c8
Joshua Parker <joshua@joshuaparker.dev>
2025-01-05 10:24:01 -0800

    Api change and other fixes.
    Signed-off-by: Joshua Parker <joshua@joshuaparker.dev>

--- CookieConsentPlugin.php
+++ CookieConsentPlugin.php
@@ -8,7 +8,6 @@ use App\Infrastructure\Services\Plugin;
 use App\Shared\Services\Registry;
 use App\Shared\Services\Utils;
 use Codefy\CommandBus\Exceptions\CommandPropertyNotFoundException;
-use Codefy\Framework\Codefy;
 use Codefy\QueryBus\UnresolvableQueryHandlerException;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
@@ -22,7 +21,6 @@ use ReflectionException;
 use function App\Shared\Helpers\add_plugins_submenu;
 use function App\Shared\Helpers\cms_enqueue_css;
 use function App\Shared\Helpers\cms_enqueue_js;
-use function App\Shared\Helpers\load_plugin_textdomain;
 use function App\Shared\Helpers\plugin_basename;
 use function App\Shared\Helpers\plugin_dir_path;
 use function App\Shared\Helpers\plugin_url;
@@ -42,11 +40,11 @@ final class CookieConsentPlugin extends Plugin
             'name' => esc_html__(string: 'Cookie Consent', domain: 'cookie-consent'),
             'id' => 'cookie-consent',
             'author' => 'Joshua Parker',
-            'version' => '1.0.0',
+            'version' => '2.0.0',
             'description' => 'Cookie Consent helps you comply with the EU regulations 
             regarding the usage of website cookies.',
             'basename' => plugin_basename(dirname(__FILE__)),
-            'path' => plugin_dir_path(__FILE__),
+            'path' => plugin_dir_path(dirname(__FILE__)),
             'url' => plugin_url('', __CLASS__),
             'pluginUri' => 'https://github.com/getdevflow/cookie-consent',
             'authorUri' => 'https://nomadicjosh.com/',
@@ -63,26 +61,12 @@ final class CookieConsentPlugin extends Plugin
      */
     public function handle(): void
     {
-        Action::getInstance()->addAction('plugins_loaded', [$this, 'setLocale']);
         Action::getInstance()->addAction('cms_head', [$this, 'enqueueFrontEndCss']);
         Action::getInstance()->addAction('cms_footer', [$this, 'enqueueFrontEndJs']);
         Action::getInstance()->addAction('cms_admin_head', [$this, 'enqueueBackEndCss']);
         Action::getInstance()->addAction('cms_admin_footer', [$this, 'enqueueBackEndJs']);
         Action::getInstance()->addAction('plugins_submenu', [$this, 'registerSubmenu']);
-        Action::getInstance()->addAction('plugins_loaded', [$this, 'render']);
-    }
-
-    /**
-     * @return void
-     * @throws ContainerExceptionInterface
-     * @throws Exception
-     * @throws InvalidArgumentException
-     * @throws NotFoundExceptionInterface
-     * @throws ReflectionException
-     */
-    public function setLocale(): void
-    {
-        load_plugin_textdomain($this->id(), $this->path() . 'locale');
+        Action::getInstance()->addAction('plugins_loaded', [$this, 'render'], 2);
     }
 
     /**
@@ -121,8 +105,7 @@ final class CookieConsentPlugin extends Plugin
             cms_enqueue_css(
                 config: 'plugin',
                 asset: $this->url() . '/assets/css/cookieconsent.min.css',
-                minify: false,
-                pluginSlug: $this->id()
+                slug: $this->id()
             );
         }
     }
@@ -143,8 +126,7 @@ final class CookieConsentPlugin extends Plugin
             cms_enqueue_js(
                 config: 'plugin',
                 asset: $this->url() . '/assets/js/cookieconsent.min.js',
-                minify: false,
-                pluginSlug: $this->id()
+                slug: $this->id()
             );
 
             if ($this->option->read('icc_popup_enabled') === 1 && $this->option->read('icc_popup_options')) {
@@ -182,8 +164,7 @@ final class CookieConsentPlugin extends Plugin
         cms_enqueue_css(
             config: 'plugin',
             asset: $this->url() . '/assets/css/admin.css',
-            minify: false,
-            pluginSlug: $this->id()
+            slug: $this->id()
         );
     }
 
@@ -212,8 +193,7 @@ final class CookieConsentPlugin extends Plugin
         cms_enqueue_js(
             config: 'plugin',
             asset: $this->url() . '/assets/js/scripts.js',
-            minify: false,
-            pluginSlug: $this->id()
+            slug: $this->id()
         );
     }
 
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 ## Description
 Cookie Consent helps you comply with the EU regulations regarding the usage of website cookies.
 
-> __Requires__ Devflow Version: 1.0.0
+> __Requires__ Devflow Version: 1.2.0
 
-> __Tested Up To:__ 1.0.0
+> __Tested Up To:__ 1.2.0
 
 > __Requires PHP:__ 8.3+
 
-> __Stable Tag:__ 1.0.0
+> __Stable Tag:__ 2.0.0
 
 > __License:__ GPLv2-only
 
@@ -24,16 +24,16 @@ Cookie Consent helps you comply with the EU regulations regarding the usage of w
 * English
 * Spanish
 
-## Composer Installation (Recommended for Devflow version 1.1.0 +)
+## Composer Installation
 1. Start a new shell session.
-2. Navigate to `public/plugins`, run the following command ```composer require getdevflow/cookie-consent```.
-
-## Git Clone Installation (Recommended for Devflow version 1.0.0)
-1. Start a shell session and navigate to ```plugins``` folder.
-2. Run the following command: ```git clone https://github.com/getdevflow/cookie-consent.git CookieConsent```.
+2. Navigate to the root of your install, run the following command ```composer require getdevflow/cookie-consent```.
 
 In your own presentation layer for a frontend, you will need to add the `App\Shared\Helpers\cms_head` function before the closing `head` tag, and the add `App\Shared\Helpers\cms_footer` function before the closing `body` tag.
 
 ## Changelog
+
+### 2.0.0
+- Api change for enqueue functions.
+
 ### 1.0.0
 - Initial release
